43308d96f8e4c6d2a027366b454fb169d4ba65d7
remonbonbon <remonbonbon@users.noreply.github.com>
2024-04-24 10:56:54 +0900

    Make graceful shutdown

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"example.com/golang-study/common"
 	"example.com/golang-study/config"
@@ -16,10 +20,31 @@ func main() {
 	logger := common.NewLogger()
 	r := web.NewRouter(logger)
 
+	srv := http.Server{Addr: conf.Listen, Handler: r}
+
+	// Graceful shutdown
+	idleConnsClosed := make(chan struct{})
+	go func() {
+		// SIGINT or SIGTERMを待機
+		sig := make(chan os.Signal, 1)
+		signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+		<-sig
+
+		// シグナル発生したらサーバーを終了
+		if err := srv.Shutdown(context.Background()); err != nil {
+			logger.Error("Shutdown failed", slog.Any("error", err))
+		}
+		logger.Info("Shutdown server")
+		close(idleConnsClosed)
+	}()
+
 	// サーバー起動
 	logger.Info(fmt.Sprintf("Listen on http://%s", conf.Listen))
-	err := http.ListenAndServe(conf.Listen, r)
-	if err != nil {
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
 		logger.Error("Listen failed", slog.Any("error", err))
+		return
 	}
+
+	// Graceful shutdown完了を待機
+	<-idleConnsClosed
 }
